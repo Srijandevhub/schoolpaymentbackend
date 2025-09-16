df822f88c01e1d35704de14c7a35eb26c73b31a7
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-09-16 16:34:05 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { login, protected, register } = require('../controllers/authControllers');
+const { login, protected, register, logout } = require('../controllers/authControllers');
 const { checkAuth } = require('../middlewares/authMiddlewares');
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/protected", checkAuth, protected);
+router.post("/logout", checkAuth, logout);
 module.exports = router;
