11878e04706bb7f66317b68940e8f7060c491bb3
Your Name <s.r.j.s.n2017@gmail.com>
2025-09-17 08:40:23 +0530

    re upload 2

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -36,7 +36,7 @@ const login = async (req, res) => {
             email: userExits.email
         }
         const accessToken = generateToken(tokenPayload, process.env.JWT_SECRET, '1d');
-        res.cookie("accesstoken", accessToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000, sameSite: "strict" });
+        res.cookie("accesstoken", accessToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
         res.status(200).json({ ok: true, message: "User loggedin" });
     } catch (error) {
         res.status(500).json({ message: "Internal Server Error", error: error.message });
