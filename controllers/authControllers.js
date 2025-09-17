29fbe5c9d96d9d53d72a41e540a4269273dd7964
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-09-17 11:09:19 +0530

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -52,7 +52,12 @@ const protected = async (req, res) => {
 }
 const logout = async (req, res) => {
     try {
-        res.clearCookie("accesstoken");
+        res.clearCookie("accesstoken", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None",
+            path: "/"
+          });
         res.status(200).json({ message: "User logged out" });
     } catch (error) {
 
